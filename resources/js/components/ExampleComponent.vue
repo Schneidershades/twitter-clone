fca7f38251c0644cb9b71aec9bdfa9febb74c668
Schneider Shades <schneidershades@gmail.com>
2021-04-07 23:50:58 +0100

    first commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        {{user}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                user: null
+            }
+        },
+
+        methods: {
+            async getUser () {
+                let response = await axios.get('/api/user')
+
+                this.user = response.data
+            }
+        },
+
+        mounted() {
+            this.getUser()
+        }
+    }
+</script>
