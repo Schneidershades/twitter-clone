091a8c72dc07170b4c61df67e5b0ba9fa11d9fcf
Schneider Shades <schneidershades@gmail.com>
2021-04-13 00:36:14 +0100

    compose style

---
+++ resources/js/components/compose/AppTweetCompose.vue
@@ -0,0 +1,14 @@
+<template>
+	<form class="flex">
+		<div>
+			avatar
+		</div>
+		<div class="flex-grow">
+			<textarea
+				class="bg-gray-900 w-full outline-none text-gray-300 text-lg resize-none mb-2"
+				placeholder="What's happening?"
+				autofocus
+			></textarea>
+		</div>
+	</form>
+</template>
