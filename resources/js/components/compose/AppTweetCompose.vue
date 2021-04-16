7a52296a24a09de8b981648aecb5e6dabe2e6f18
Schneider Shades <schneidershades@gmail.com>
2021-04-16 16:50:24 +0100

    push

--- resources/js/components/compose/AppTweetCompose.vue
+++ resources/js/components/compose/AppTweetCompose.vue
@@ -4,12 +4,9 @@
 			<img :src="$user.avatar" class="w-12 rounded-full">
 		</div>
 		<div class="flex-grow">
-			<textarea
-				class="bg-gray-900 w-full outline-none text-gray-300 text-lg resize-none mb-2"
-				placeholder="What's happening?"
+			<app-tweet-compose-textarea
 				v-model="form.body"
-				autofocus
-			></textarea>
+			/>
 
 			<div class="flex justify-between">
 				<div>
@@ -43,6 +40,7 @@
 		methods : {
 			async submit () {
 				await axios.post('/api/tweets', this.form)
+				this.form.body = ''
 			},
 		}
 	}
--- resources/js/components/tweets/variants/AppTweetVariantTweet.vue
+++ resources/js/components/tweets/variants/AppTweetVariantTweet.vue
@@ -6,6 +6,22 @@
 		<div class="flex-grow">
 			<app-tweet-username :user="tweet.user" />
 			<p class="text-gray-300 whitespace-pre-wrap">{{ tweet.body }}</p>
+
+			<ul class="flex mt-4">
+				<li class="w-3/12">
+					<a href="">
+						icon
+					 	<span></span>
+					</a>
+				</li>
+
+				<li class="w-3/12">
+					 retweet
+				</li>
+				<li class="w-3/12">
+					 like
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
