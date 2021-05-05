dd1eff86b12ded0d8e5f0533513d8d103374abd3
Schneider Shades <schneidershades@gmail.com>
2021-05-05 10:54:37 +0100

    realtime likes

---
+++ app/Events/Tweet/TweetLikesWereUpdated.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Events\Tweet;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\Tweet;
+use App\Models\User;
+
+class TweetLikesWereUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $user;
+    protected $tweet;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, Tweet $tweet)
+    {
+        $this->tweet = $tweet;
+        $this->user = $user;
+    }
+
+    public function broadcastAs()
+    {
+        return 'TweetLikesWereUpdated';
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'id' => $this->tweet->id,
+            'user_id' => $this->user->id,
+            'count' => $this->tweet->likes->count(),
+        ];
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('tweets');
+    }
+}
